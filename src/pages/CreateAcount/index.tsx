e72ecd3af908940722330d108e9fcead1d58ce1f
Eduardo Silva <eduardojrodriguessilva@gmail.com>
2022-08-16 13:32:27 -0300

    removing unused import

--- src/pages/CreateAcount/index.tsx
+++ src/pages/CreateAcount/index.tsx
@@ -5,7 +5,6 @@ import { useToast } from '../../providers/Toast';
 import api from '../../services/api';
 
 import { Input } from '../../components/Input/styles';
-import { Space } from '../../components/Space/styles';
 
 import {
   Button,
